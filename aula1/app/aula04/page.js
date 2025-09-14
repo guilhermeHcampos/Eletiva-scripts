26012d0f5e0b2cae7a0f5a854ea2b5c5e9fba9da
guilhermeHcampos <guistrong8@gmail.com>
2025-09-13 22:28:41 -0300

    Adiciona a nova pasta [nome-da-pasta]

---
+++ aula1/app/aula04/page.js
@@ -0,0 +1,25 @@
+"use client"
+import { useState } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const [valor, setValor] = useState(0);
+
+    function handleMouseMove(event) {
+        setX(event.clientX);
+        setY(event.clientY);
+    }
+
+    return (
+        <div className={styles.page} onMouseMove={handleMouseMove}>
+            <span>Eixo X: {x}</span>
+            <span>Eixo Y: {y}</span>
+            <span>Número: {valor}</span>
+            <button onClick={() => setValor(valor + 1)}>+ 1</button>
+            <button onClick={() => setValor(valor - 1)}>- 1</button>
+        </div>
+    );
+}
